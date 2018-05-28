b8688764804bbb844c4e4c83e00bc68123c645c2
Andrea <marco.minniti@consulenti.iblbanca.it>
2018-05-28 16:08:22 +0200

    modifiche a footer e header

--- src/components/mainLayout/footer/footerCV.js
+++ src/components/mainLayout/footer/footerCV.js
@@ -4,7 +4,7 @@ import "../../../css/mainLayout/footer/footer.css"
 export default class FooterCV extends React.Component{
     render(){
         return(
-            <div className="cv">
+            <div className="cv col-md-4">
                 <h4>Scarica il mio CV!</h4>
             </div>
         );
--- src/components/mainLayout/footer/footerContact.js
+++ src/components/mainLayout/footer/footerContact.js
@@ -4,8 +4,10 @@ import "../../../css/mainLayout/footer/footer.css"
 export default class FooterContact extends React.Component{
     render(){
         return(
-            <div className="footer-social-icons">
+            <div className="footer-social-icons col-md-4">
                 <h4>I miei contatti</h4>
+                <div className="footer-social-content">
+                </div>
                 <ul className="social-icons">
                     <li><a href="https://www.facebook.com/andrea.minniti7" className="social-icon"><span>Facebook</span><i className="fa fa-facebook"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/marco-andrea-minniti-ab6564112/" className="social-icon"><span>Linkedin</span><i className="fa fa-linkedin"></i></a></li>
